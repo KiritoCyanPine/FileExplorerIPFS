e3504d55c085df696ff2e8843b4ecd221e4ad2f0
Debasish Nandi <debasish.nandi-intl@cylogic.com>
2023-06-07 23:55:51 +0530

    Initial Commit

---
+++ FileExplorerIPFS/FileExplorerIPFSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileExplorerIPFSApp.swift
+//  FileExplorerIPFS
+//
+//  Created by Debasish Nandi on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FileExplorerIPFSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
